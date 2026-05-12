a36d0b301ced6f5f555c1102f175f820613667f3
qingqiao1223 <2471855584@qq.com>
2026-05-12 17:44:02 +0800

    Delete firstcc-pb directory

--- firstcc-pb/app/auth/signout/route.ts
+++
@@ -1,8 +0,0 @@
-import { createServerClient } from "@/lib/pocketbase/server";
-import { NextResponse } from "next/server";
-
-export async function POST(request: Request) {
-  const pb = await createServerClient();
-  pb.authStore.clear();
-  return NextResponse.redirect(new URL("/", request.url));
-}
